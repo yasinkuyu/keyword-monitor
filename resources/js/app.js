bee7684ca1b36ba84ec45a29cbe3272e9a33e3c7
Yasin Kuyu <yasinkuyu@gmail.com>
2024-06-09 19:31:12 +0300

    Configure progress bar appearance and behavior for page transitions

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,6 +18,16 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        // The delay after which the progress bar will appear, in milliseconds...
+        delay: 250,
+
+        // The color of the progress bar...
+        color: '#29d',
+
+        // Whether to include the default NProgress styles...
+        includeCSS: true,
+
+        // Whether the NProgress spinner will be shown...
+        showSpinner: false,
     },
 });
